f986b5782508b33438c9142ee3d76bf4c45f0d1d
Michelle C. Funk <michelle@michellefunk.com>
2015-05-07 15:11:32 -0500

    Original HTML displays

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+//Configuration file for Grunt
+module.exports = function(grunt) {
+    //Project configuration
+    //Pass in object to .initConfig
+    grunt.initConfig ({
+        //First, reference package.json file needed for config
+        pkg: grunt.file.readJSON('package.json'),
+        //Configure options for uglify, which will compress code for faster computer reading.
+        uglify: {
+            options: {
+                //Note use of pkg here -- need package.json to print pkg.name
+                banner: '/*! <%pkg.name%><%=grunt.template.today("yyyy-mm-dd")%>*/\n'
+            },
+            //Define source and destination for uglified code
+            build: {
+                src:'client/app.js',
+                dest: 'server/public/assets/scripts/app.min.js'
+            }
+        },
+        //Configure options for copy, which will copy needed node modules into
+        // client-side-usable location(?). Specifically in this case, Angular will be needed.
+        copy: {
+            main: {
+                expand: true,
+                //Current working directory: where to find node modules to copy
+                cwd: "node_modules",
+                src: [
+                    "bootstrap/dist/css/bootstrap.min.css",
+                    "bootstrap/dist/js/bootstrap.min.js",
+                    "jquery/dist/jquery.min.js",
+                    "jquery/dist/jquery.min.map"
+                ],
+                "dest": "server/public/vendor"
+            },
+            css: {
+                expand: true,
+                cwd: "client/",
+                src: "stylesheet.css",
+                "dest": "server/public/assets/css"
+            }
+        }
+    });
+
+    //Register needed tasks (uglify and copy) for running Grunt
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //Default task(s)
+    grunt.registerTask('default', ['copy', 'uglify']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-
+Project Scope
+-  Create resume website that improves upon existing navigation in mc-funk.github.io
+-  Use Bootstrap
+-  Use jQuery
