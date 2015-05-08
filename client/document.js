1c9df5a5c192624679996090b679f194d27c4a90
Michelle C. Funk <michelle@michellefunk.com>
2015-05-08 11:41:57 -0500

    Add dynamic sidebar height

--- client/document.js
+++ client/document.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
+    $.get("/assets/scripts/profile.html", function(data) {
+        console.log(data);
+        $(".textArea").html(data);
+        setHeight();
+    });
+
     $("#profile").on("click", function() {
         $.get("/assets/scripts/profile.html", function(data){
             console.log(data);
-            $(".mainContainer").html(data);
+            $(".textArea").html(data);
+            setHeight();
         });
     });
 
     $("#employment").on("click", function() {
         $.get("/assets/scripts/employment.html", function(data){
             console.log(data);
-            $(".mainContainer").html(data);
+            $(".textArea").html(data);
+            setHeight();
         });
     });
 
     $("#education").on("click", function() {
         $.get("/assets/scripts/education.html", function(data){
             console.log(data);
-            $(".mainContainer").html(data);
+            $(".textArea").html(data);
+            setHeight();
         });
     });
 });
+
+function setHeight(){
+    var eHeight = $(".textArea").innerHeight();
+    $(".sidebar").outerHeight(eHeight);
+}
