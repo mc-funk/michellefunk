33074242a88110b52e05646cd78712f01eb41ed3
Michelle C. Funk <michelle@michellefunk.com>
2015-05-08 10:28:45 -0500

    add nav button population

--- client/document.js
+++ client/document.js
@@ -1,1 +1,22 @@
-$('.resume').text("JQUERY WORKS");
+$(document).ready(function() {
+    $("#profile").on("click", function() {
+        $.get("/assets/scripts/profile.html", function(data){
+            console.log(data);
+            $(".mainContainer").html(data);
+        });
+    });
+
+    $("#employment").on("click", function() {
+        $.get("/assets/scripts/employment.html", function(data){
+            console.log(data);
+            $(".mainContainer").html(data);
+        });
+    });
+
+    $("#education").on("click", function() {
+        $.get("/assets/scripts/education.html", function(data){
+            console.log(data);
+            $(".mainContainer").html(data);
+        });
+    });
+});
