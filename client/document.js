597206db2e3a520026c33e9970965dcc7b0cca90
Michelle C. Funk <michelle@michellefunk.com>
2017-02-05 16:33:38 -0600

    Content and styling updates (#6)
    * Update front page content; responsiveness, styling edits
    
    * Remove community tab for now
    
    * Add softBlack font color, eliminiate use of <em>, layout tweaks

--- client/document.js
+++ client/document.js
@@ -1,69 +1,59 @@
 $(document).ready(function() {
-    $.get("/grav", {name:'michelle@michellefunk.com'}, function(gravatarUrl) {
-        $.ajax({
-                type: 'GET',
-                dataType: 'jsonp',
-                crossDomain: true,
-                url: gravatarUrl,
-                complete: function() {
-                    console.log("Ajax call complete")
-                },
-                success: function(data) {
-                    var profile = data.entry[0];
-                    console.log(data);
-                    $(".gravatar").append("<a href='" + profile.profileUrl + "'> " +
-                        "<img class='img-bordered img-responsive' src='" + profile.photos[0].value + "?s=150&d=identicon&r=G'></a>");
-                    $(".gravatar").append("<h4 class='white'>" + profile.displayName + "<br></h5>");
-                    $(".gravatar").append("<h5><em>" + profile.currentLocation + "<br></em></h5>");
-                    $(".gravatar").append("<h5>" + profile.aboutMe + "</h5>");
-                },
-                error: function(xhr, status) {
-                    console.log("Error : ", xhr, " ", status);
-                }
-        });
+  $.get("/grav", {name:'michelle@michellefunk.com'}, function(gravatarUrl) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      crossDomain: true,
+      url: gravatarUrl,
+      success: function(data) {
+        var profile = data.entry[0];
+        $(".gravatar").append("<a href='" + profile.profileUrl + "'> " +
+        "<img class='img-bordered img-responsive' src='" + profile.photos[0].value + "?s=150&d=identicon&r=G'></a>");
+        $(".gravatar").append("<h4 class='white'>" + profile.displayName + "<br></h5>");
+        $(".gravatar").append("<h5><em>" + profile.currentLocation + "<br></em></h5>");
+        $(".gravatar").append("<h5>" + profile.aboutMe + "</h5>");
+      },
+      error: function(xhr, status) {
+        console.log("Error : ", xhr, " ", status);
+      }
     });
+  });
 
-    $.get("/assets/scripts/profile.html", function(data) {
-        $(".textArea").html(data);
-        setHeight();
+  $.get("/assets/scripts/profile.html", function(profile) {
+    $(".textArea").html(profile);
+    setHeight();
+  });
+
+  $("#profile").on("click", function() {
+    $.get("/assets/scripts/profile.html", function(profile){
+      $(".textArea").html(profile);
+      setHeight();
     });
+  });
 
-    $("#profile").on("click", function() {
-        $.get("/assets/scripts/profile.html", function(data){
-            $(".textArea").html(data);
-            setHeight();
-        });
+  $("#employment").on("click", function() {
+    $.get("/assets/scripts/employment.html", function(employment){
+      $(".textArea").html(employment);
+      setHeight();
     });
+  });
 
-    $("#employment").on("click", function() {
-        $.get("/assets/scripts/employment.html", function(data){
-            $(".textArea").html(data);
-            setHeight();
-        });
+  $("#education").on("click", function() {
+    $.get("/assets/scripts/education.html", function(education){
+      $(".textArea").html(education);
+      setHeight();
     });
+  });
 
-    $("#education").on("click", function() {
-        $.get("/assets/scripts/education.html", function(data){
-            $(".textArea").html(data);
-            setHeight();
-        });
+  $("#community").on("click", function() {
+    $.get("/assets/scripts/community.html", function(community){
+      $(".textArea").html(community);
+      setHeight();
     });
-
-    $("#community").on("click", function() {
-        $.get("/assets/scripts/community.html", function(data){
-            $(".textArea").html(data);
-            setHeight();
-        });
-    });
-
-
+  });
 });
 
 function setHeight(){
-    var eHeight = $(".textArea").innerHeight();
-    $(".sidebar, .sidebar2").outerHeight(eHeight);
-}
-
-function displayGravatar() {
-    console.log("Will run function")
+  var eHeight = $(".textArea").innerHeight();
+  $(".sidebar, .sidebar2").outerHeight(eHeight);
 }
--- server/gravatar.js
+++ server/gravatar.js
@@ -5,7 +5,6 @@ function hash(input) {
     input = input.toString();
     input = input.trim();
     input = input.toLowerCase();
-    console.log("https://www.gravatar.com/" + CryptoJS(input) + ".json?callback=displayGravatar");
     return "https://www.gravatar.com/" + CryptoJS(input) + ".json?callback=displayGravatar";
 }
 
